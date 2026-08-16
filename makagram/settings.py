caa66c6e85b67f3128643962ad3d107752fbfabb
Makar Hysko <hysko.mm@gmail.com>
2026-08-16 12:28:53 +0300

    rename chats feature to rooms, change styles

--- makagram/settings.py
+++ makagram/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','
 
 INSTALLED_APPS = [
     'daphne',
-    'chat',
+    'room',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- makagram/urls.py
+++ makagram/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path('chat/', include('chat.urls')),
+    path('', views.index, name='index'),
+    path('room/', include('room.urls')),
     path('admin/', admin.site.urls),
 ]
